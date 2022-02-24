2e37a2f9e6024c39edb4a10aa766ffac5fa570c8
Kendra Wilkerson <kwilkerson@ggc.edu>
2022-02-24 11:43:32 -0500

    added main menu

---
+++ phonebook.py
@@ -0,0 +1,12 @@
+#PRACTICING WORKING THROUGH THE PHONE BOOK APP
+
+
+
+
+print("ELECTRONIC PHONE BOOK") 
+print("1. Look up an entry")
+print("2. Set an entry")
+print("3. Delete an entry")
+print("4. List all entries")
+print("5. Quit")
+print("How can I help you?")
