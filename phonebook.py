337423646d153cb09fb238a75c19aceb8d90bce8
Kendra Wilkerson <kwilkerson@ggc.edu>
2022-02-25 12:09:53 -0500

    this update will allow the user to look up the corresponding number for a given name

--- phonebook.py
+++ phonebook.py
@@ -3,6 +3,9 @@
 
 
 
+
+
+
 print("ELECTRONIC PHONE BOOK") 
 print("1. Look up an entry")
 print("2. Set an entry")
@@ -11,17 +14,63 @@ print("4. List all entries")
 print("5. Quit")
 print("How can I help you?")
 
-person = {
-    "first": "Kendra",
-    "second": "Kru",
-    "third": "Dominique"
+
+name = {
+    "name1": "Kendra",
+    "name2": "Kru",
+    "name3": "Dominique"
 }
 number = {
-    "first": "111-111-1111",
-    "second": "222-222-2222",
-    "third": "333-333-3333"
+    "num1": "111",
+    "num2": "222",
+    "num3": "333"
 }
+
+#THIS BLOCK OF CODE WILL ALLOW THE USER TO LOOK UP THE CORRESPONDING NUMBER FOR A GIVEN NAME 
+name = input("Enter name:")
+#a = number.get("num2")
+#print(a)
+x = number.get("num1")
+if name == "Kendra":
+    print(x)
+
+y = number.get("num2")
+if name == "Kru":
+    print(y)
+
+z = number.get("num3")
+if name == "Dominique":
+    print(z)
+
+
+
+"""
+name1 = "Kendra"
+name2 = "Kru"
+name3 = "Dominique"
+
+number1 = "111"
+number2 = "222"
+number3 = "333"
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #THIS BLOCK OF CODE WILL ALLOW THE USER TO SET/SAVE A NEW CONTACT
+
 def entries():
     names = ["Kendra", "Kru", "Dominique"]
     
@@ -43,5 +92,6 @@ def contactinfo():
         numbers.append(b)
         print(numbers)
 contactinfo()
+    #elif ()
 
 
